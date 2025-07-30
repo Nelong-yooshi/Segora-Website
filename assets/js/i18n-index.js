c8912f1ac31c879a264d4f0710a425f336925157
Rlong <rlooooong@gmail.com>
2025-07-31 01:29:50 +0800

    Add Core Principle

--- assets/js/i18n-index.js
+++ assets/js/i18n-index.js
@@ -11,53 +11,10 @@ function loadLanguage(lang) {
       document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.getAttribute('data-i18n');
         if (data[key]) {
-          el.innerHTML = data[key];
+          el.textContent = data[key];
         }
       });
-
-
-      setTimeout(() => resetSplitTextAnimation(), 0);
     });
-}
-
-let split;
-let tl;
-
-function resetSplitTextAnimation() {
-  if (tl) {
-    tl.kill();
-    tl = null;
-  }
-
-  ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-
-  if (split) {
-    split.revert();
-    split = null;
-  }
-
-  split = new SplitText(".wrapper-split p", {
-    type: "words",
-    wordsClass: "word"
-  });
-
-  tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#textSection",
-      start: "top-=100 top",
-      end: "+=200%",
-      pin: true,
-      scrub: true,
-      markers: false
-    }
-  });
-
-  tl.set(split.words, {
-    opacity: 1,
-    stagger: 0.1
-  }, 0.1);
-
-  tl.to({}, { duration: 1 });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
